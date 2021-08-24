cab25a0201813e402713258abf5fef14974afbae
Eric Liu <ericyl.us@gmail.com>
2021-08-24 06:40:37 -0700

    chore: add example set-ups

---
+++ examples/webpack/src/App.svelte
@@ -0,0 +1,51 @@
+<script>
+  import Time, { svelteTime } from "svelte-time";
+  import { dayjs } from "svelte-time";
+
+  let time = dayjs().format("MMM DD, YYYY");
+</script>
+
+<h2>Basic</h2>
+
+<Time />
+<Time timestamp="2020-02-01" />
+<Time timestamp="{new Date()}" format="dddd @ h:mm a" />
+<Time timestamp="{1e10}" format="dddd @ h:mm A · MMMM D, YYYY" />
+
+<h2>Relative</h2>
+
+<Time relative />
+<Time relative timestamp="2021-02-02" />
+<Time relative timestamp="{1e10}" />
+<Time live relative />
+
+<h2>use:svelteTime</h2>
+
+<time use:svelteTime></time>
+
+<time
+  use:svelteTime="{{
+    timestamp: '2021-02-02',
+    format: 'dddd @ h:mm A · MMMM D, YYYY',
+  }}"></time>
+
+<time
+  use:svelteTime="{{
+    relative: true,
+    timestamp: '2021-02-02',
+  }}"></time>
+
+<time
+  use:svelteTime="{{
+    relative: true,
+    live: true,
+  }}"></time>
+
+{time}
+
+<style>
+  :global(time) {
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+</style>
