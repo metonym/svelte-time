677ebdb399c95951c35172a3111bd316243fde29
metonym <ericyl.us@gmail.com>
2022-10-20 08:43:39 -0700

    chore(deps-dev): bump deps and add @rollup/plugin-commonjs

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,7 @@
 import svelteReadme from "svelte-readme";
+import commonjs from "@rollup/plugin-commonjs";
 
 export default svelteReadme({
   style: `time { display: block; margin-bottom: 0.5rem; }`,
+  plugins: [commonjs()],
 });
