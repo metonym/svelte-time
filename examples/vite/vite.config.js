5cd35eef1522b960ce5c07ad93d1a0e7a42b5492
Eric Liu <ericyl.us@gmail.com>
2022-12-11 11:05:06 -0800

    chore(examples): bump deps

--- examples/vite/vite.config.js
+++ examples/vite/vite.config.js
@@ -3,7 +3,4 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 /** @type {import('vite').UserConfig} */
 export default {
   plugins: [svelte()],
-  optimizeDeps: {
-    include: ["dayjs/plugin/relativeTime.js"],
-  },
 };
