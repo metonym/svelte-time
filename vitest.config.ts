f30ede179af9be24f39e2f764900d52369b83f5b
metonym <ericyl.us@gmail.com>
2023-07-30 09:02:04 -0700

    chore(config): convert to TS

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
-import { svelte } from "@sveltejs/vite-plugin-svelte";
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 import pkg from "./package.json";
 
 export default defineConfig({
-  plugins: [svelte({ hot: false })],
+  plugins: [svelte({ hot: false, preprocess: [vitePreprocess()] })],
   resolve: {
     alias: {
       [pkg.name]: pkg.main,
