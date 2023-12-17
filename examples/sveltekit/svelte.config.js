5064a0e50345e2d2cdae8e1a2268a27a61dc23b2
Eric Liu <ericyl.us@gmail.com>
2023-12-16 17:15:59 -0800

    chore(examples): bump deps [ci skip]

--- examples/sveltekit/svelte.config.js
+++ examples/sveltekit/svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from "@sveltejs/adapter-node";
-import { vitePreprocess } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-  preprocess: [vitePreprocess()],
+  preprocess: vitePreprocess(),
   kit: {
     adapter: adapter(),
   },
