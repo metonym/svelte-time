9d1f2b97ef32d033b9de48fb1e8814ddd7048dfa
Eric Liu <ericyl.us@gmail.com>
2025-08-10 12:45:23 -0700

    test: move `SvelteTimeAction` to tests folder

--- tests/SvelteTimeAction.svelte
+++ tests/SvelteTimeAction.svelte
@@ -1,11 +1,11 @@
-<script>
+<script lang="ts">
   import { svelteTime } from "svelte-time";
 
   let timestamp = $state("2021-02-02");
   let format = $state("dddd @ h:mm A · MMMM D, YYYY");
 </script>
 
-<button onclick={() => (timestamp = "2022-02-02")}> Update </button>
+<button onclick={() => (timestamp = "2022-02-02")}>Update</button>
 
 <div>
   <time use:svelteTime={{ timestamp, format }}></time>
