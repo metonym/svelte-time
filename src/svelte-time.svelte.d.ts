bc55c0b6ac357145e51689ede1ce7f36c9c0da92
Eric Liu <ericyl.us@gmail.com>
2025-11-30 22:13:41 -0800

    chore(deps-dev): bump deps

--- src/svelte-time.svelte.d.ts
+++ src/svelte-time.svelte.d.ts
@@ -1,11 +1,10 @@
 import type { Action } from "svelte/action";
 import type { TimeProps } from "./Time.svelte";
 
-export interface SvelteTimeOptions
-  extends Pick<
-    TimeProps,
-    "timestamp" | "format" | "relative" | "live" | "title"
-  > {}
+export interface SvelteTimeOptions extends Pick<
+  TimeProps,
+  "timestamp" | "format" | "relative" | "live" | "title"
+> {}
 
 export const svelteTime: Action<
   HTMLElement,
